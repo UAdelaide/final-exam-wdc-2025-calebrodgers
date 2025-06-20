6cf7534539d56f70a3b29e65d1e1fb994ef5e316
Caleb Rodgers <24555386+calebrodgers@users.noreply.github.com>
2025-06-20 09:27:20 +0000

    [automatic commit] 2025-06-20 09:27:19 UTC 60.225.224.204

--- part1/app.js
+++ part1/app.js
@@ -10,7 +10,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const db = mysql.createConnection({
     host: "localhost",
