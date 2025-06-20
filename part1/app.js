21cfc6c717b8d37fc8ac8431158110a41d921320
Caleb Rodgers <24555386+calebrodgers@users.noreply.github.com>
2025-06-20 10:19:21 +0000

    [automatic commit] 2025-06-20 10:19:21 UTC 60.225.224.204

--- part1/app.js
+++ part1/app.js
@@ -51,7 +51,7 @@ app.get('/api/walkrequests/open', async (req, res) => {
 // Route to return a summary of each walker with their average rating and number of completed walks.
 app.get('/api/walkers/summary', async (req, res) => {
     try {
-        const [walkers_summary] = await db.query(`SELECT u.username `);
+        const [walkers_summary] = await db.query(`SELECT u.username AS walker_username, COUNT(r.request_id )`);
         res.json(walkers_summary);
     } catch (err) {
         res.status(500).json({ error: 'Failed to fetch walkers summary' });
