3d062e48518895c423b4d6da3852ad0e9d0f8889
Caleb Rodgers <24555386+calebrodgers@users.noreply.github.com>
2025-06-20 09:40:38 +0000

    [automatic commit] 2025-06-20 09:40:38 UTC 60.225.224.204

--- part1/app.js
+++ part1/app.js
@@ -15,16 +15,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 let db;
 
 (async () => {
-    try{
-    db = await mysql.createConnection({
-        host: "localhost",
-        user: 'root',
-        password: '',
-        database: "DogWalkService"
-    });
-} catch (err) {
-    console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
-  }
+    try {
+        db = await mysql.createConnection({
+            host: "localhost",
+            user: 'root',
+            password: '',
+            database: "DogWalkService"
+        });
+    } catch (err) {
+        console.error('Error connecting to database', err);
+    }
 });
 
 // db.connect((err) => {
