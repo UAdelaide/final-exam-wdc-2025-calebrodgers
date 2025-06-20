bb9f089981ba41bc219e20a1883a9e9889204bc8
Caleb Rodgers <24555386+calebrodgers@users.noreply.github.com>
2025-06-20 10:08:29 +0000

    [automatic commit] 2025-06-20 10:08:29 UTC 60.225.224.204

--- part1/app.js
+++ part1/app.js
@@ -41,7 +41,7 @@ app.get('/api/walkrequests/open', async (req, res) => {
         const [requests] = await db.query(`SELECT w.request_id, d.name AS dog_name, w.requested_time, w.location, u.username AS owner_username fROM WalkRequests w JOIN Dogs d ON w.dog_id = d.dog_id JOIN Users u ON d.owner_id = u.user_id WHERE w.status = 'open'`);
         res.json(requests);
     } catch (err) {
-        res.status(500).json({ error: 'Failed to open walk requests dogs' });
+        res.status(500).json({ error: 'Failed to open walk requests' });
     }
 });
 
