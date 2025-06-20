e4bfeef8b38b1bf8168466a4237f3573f3d22f54
Caleb Rodgers <24555386+calebrodgers@users.noreply.github.com>
2025-06-20 09:28:20 +0000

    [automatic commit] 2025-06-20 09:28:20 UTC 60.225.224.204

--- part1/app.js
+++ part1/app.js
@@ -28,6 +28,7 @@ app.get('/', async (req, res) => {
         const [users] = await db.execute('SELECT * FROM Users');
         res.json(users);
     } catch (err) {
+        console.log(err);
         res.status(500).json({ error: 'Failed to fetch users' });
     }
 });
