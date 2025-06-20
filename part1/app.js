e46ce24e5831fe59be534e272e4114856b20acb5
Caleb Rodgers <24555386+calebrodgers@users.noreply.github.com>
2025-06-20 09:20:11 +0000

    [automatic commit] 2025-06-20 09:20:11 UTC 60.225.224.204

--- part1/app.js
+++ part1/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mysql = require('mysql2/');
+var mysql = require('mysql2');
 
 var app = express();
 
