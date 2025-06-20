38c0a3999c738d65f08652d2540c199adac7fd86
Caleb Rodgers <24555386+calebrodgers@users.noreply.github.com>
2025-06-20 14:02:35 +0000

    [automatic commit] 2025-06-20 14:02:35 UTC 60.225.224.204

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
   try {
     const [rows] = await db.query(`
       SELECT dog_id, name FROM Dogs WHERE owner_id = ?
-    `, req.sess);
+    `, req.session.user.user_id);
     res.json(rows);
   } catch (error) {
     console.error('SQL Error:', error);
