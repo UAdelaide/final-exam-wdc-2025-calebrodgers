a8c09b3ac411871e2672efa85c157379b50301dc
Caleb Rodgers <24555386+calebrodgers@users.noreply.github.com>
2025-06-20 12:50:11 +0000

    [automatic commit] 2025-06-20 12:50:11 UTC 60.225.224.204

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -71,7 +71,7 @@ router.post('/logout', (req, res) => {
       return res.status(500).json({ error: 'Logout error' });
     }
 
-    res.json({message:''})
+    res.redirect("index.html");
   });
 });
 
