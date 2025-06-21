0d145035c81da55eeadb646754073593fc74f337
Caleb Rodgers <24555386+calebrodgers@users.noreply.github.com>
2025-06-21 02:31:45 +0000

    [automatic commit] 2025-06-21 02:31:45 UTC 14.2.198.171

--- part2/app.js
+++ part2/app.js
@@ -19,13 +19,15 @@ app.use(
 );
 
 // Redirect all unauthenticated users to index, and owners and walkers
-// to their respective pages
+// to their respective dashboards
 app.use((req, res, next) => {
     const exposedPaths = ["/index.html", "/api/users/login", "/api/dogs"];
 
     if (exposedPaths.includes(req.path)) {
         return next();
     }
+
+    
 
     if (!req.session.user) {
         return res.redirect('/index.html');
