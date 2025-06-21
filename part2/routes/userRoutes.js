309969b9b38c7189125e98550daad4ca1229806e
Caleb Rodgers <24555386+calebrodgers@users.noreply.github.com>
2025-06-21 04:42:36 +0000

    [automatic commit] 2025-06-21 04:42:36 UTC 14.2.198.171

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -16,7 +16,10 @@ router.get('/', async (req, res) => {
 router.post('/register', async (req, res) => {
   const {
     username,
-    email, password, role } = req.body;
+    email,
+    password,
+    role
+  } = req.body;
 
   try {
     const [result] = await db.query(`
@@ -34,7 +37,7 @@ router.get('/me', (req, res) => {
   if (!req.session.user) {
     return res.status(401).json({ error: 'Not logged in' });
   }
-  res.json(req.session.user);
+  return res.json(req.session.user);
 });
 
 // POST login
