ecb86eb10abf48f40983741a5272fa6c8593ab1b
Caleb Rodgers <24555386+calebrodgers@users.noreply.github.com>
2025-06-21 02:24:16 +0000

    [automatic commit] 2025-06-21 02:24:16 UTC 14.2.198.171

--- part2/app.js
+++ part2/app.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
         }
 
         if (req.path === "/walker-dashboard.html") {
-            return res.redirect
+            return res.redirect('/owner-dashboard.html');
         }
     }
 
